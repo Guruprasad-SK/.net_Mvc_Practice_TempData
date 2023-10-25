6aedaa3804f3e99be481ab402427b552a93f62b4
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-10-25 23:55:12 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempDataTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index1()
+        {
+            TempData["MyKey"] = "This My Key data";
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            Session.Abandon();
+            ViewBag.MyKeyData = TempData.Peek("MyKey");
+           // TempData.Keep("MyKey");
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            ViewBag.MyKeyData = TempData["MyKey"];
+            return View();
+        }
+    }
+}
